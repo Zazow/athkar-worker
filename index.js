b8da99072d4ccee5a00be784443c5aaa0c0d8c47
Ziyad <ziyadkag@gmail.com>
2025-01-27 18:51:20 +0300

    update code to pull

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import { Router } from 'itty-router';
 import jwt from '@tsndr/cloudflare-worker-jwt';
-
-const router = Router();
 
 const validateString = value => {
 	return Boolean(Number(value)) || value === 'true' || value === 'false' || value.charAt(0) === "=" || value.charAt(0) === "+";
@@ -46,47 +43,30 @@ async function getGoogleSheetsAccessToken(env) {
 	return response.access_token;
 }
 
-async function addRow(payload, accessToken, env) {
+async function getAllRows(accessToken, env) {
 	try {
 		const response = await fetch(
-			`https://sheets.googleapis.com/v4/spreadsheets/${env.GOOGLE_SHEETS_SUBSCRIBERS_ID}/values/${env.GOOGLE_SHEETS_SUBSCRIBERS_PAGE}:append?valueInputOption=USER_ENTERED`,
+			`https://sheets.googleapis.com/v4/spreadsheets/${env.GOOGLE_SHEETS_ID}/values/${env.GOOGLE_SHEETS_PAGE}?majorDimension=ROWS`,
 			{
-				method: 'POST',
+				method: 'GET',
 				headers: {
-					'Content-Type': 'application/json',
 					'Authorization': `Bearer ${accessToken}`,
 				},
-				body: JSON.stringify({
-					range: env.GOOGLE_SHEETS_SUBSCRIBERS_PAGE,
-					majorDimension: 'ROWS',
-					values: [
-						[
-							payload.name,
-							payload.email,
-							payload.subject,
-							payload.message,
-							new Date().getDate() +
-								'/' +
-								(new Date().getMonth() + 1) +
-								'/' +
-								new Date().getFullYear(),
-							new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds(),
-							"=OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),0,-1) + TIME(5, 30, 0)",
-						],
-					],
-				}),
 			}
 		);
-		if (response.ok)
+		
+		if (response.ok) {
+			const data = await response.json();
 			return {
-				data: 'Success! Your Message has been recorded. I will get back to you soon.',
+				data: data.values, // Pull all rows as an array of arrays
 				status: response.status,
 			};
-		else
+		} else {
 			return {
 				error: response.statusText,
 				status: response.status,
 			};
+		}
 	} catch (error) {
 		return {
 			error: error,
@@ -94,31 +74,15 @@ async function addRow(payload, accessToken, env) {
 	}
 }
 
-router.get('/', () => {
-	return Response.redirect('https://shreyvarshney.pages.dev', 302);
-});
-
-router.post('/post', async (request, env) => {
-	const data = await request.json();
-    if(!validateEmail(data.email) || validateString(data.name) || validateString(data.subject) || validateString(data.message)){
-        return new Response('Invalid Input', { status: 400 });
-    }
-	const accessToken = await getGoogleSheetsAccessToken(env);
-	let response = await addRow(data, accessToken, env);
-	if (response === null || typeof response === 'undefined') {
-		return new Response('Error adding row to Google Sheets', { status: 500 });
-	}
-	response = new Response(response.data, { status: response.status });
-	response.headers.set('Access-Control-Allow-Origin', '*');
-	response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-	return response;
-});
-
-router.all('/', () => new Response('404, not found!', { status: 404 }));
 
 export default {
 	async fetch(request, env) {
-		const response = await router.handle(request, env);
-		return response;
+    const accessToken = await getGoogleSheetsAccessToken(env);
+    const rows = await getAllRows(accessToken, env);
+		return new Response(JSON.stringify(rows), {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 	},
 };
